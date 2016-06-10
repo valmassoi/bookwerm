405d4ee2ab3216783da267625543f342a62e4849
valmassoi <rvalmassoi@gmail.com>
2016-06-10 15:03:15 -0700

    heroku test

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ const app = express()
 const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27018/data'
 mongoose.connect(dbUrl)
 
-app.use("/", express.static(__dirname))
 app.use(express.static(__dirname+'/src/'))//TODO
 app.use(morgan('combined'))
 app.use(cors())
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ type: '*/*' }))
-app.enable('trust proxy')
+// app.enable('trust proxy')
 router(app)
 
 
